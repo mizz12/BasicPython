930381bd2d0e4d100647f43ba7229a20924f165d
mizz12 <w.mizuno.23.10.1208@4nonome.com>
2023-12-05 19:42:58 +0900

    (2 * a)に変更しました。

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -4,8 +4,8 @@ c = 9
 
 # TODO
 def formula(a,b,c):
-    x1 = ( - b + (b ** 2 - 4 * a * c) ** (1 / 2)) / 2 * a
-    x2 = ( - b - (b ** 2 - 4 * a * c) ** (1 / 2)) / 2 * a
+    x1 = ( - b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
+    x2 = ( - b - (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
     return x1,x2
 
 print(formula(1,-6,9))
