e1497d403cce1310694dc80519f5e75193090eb2
mizz12 <w.mizuno.23.10.1208@4nonome.com>
2023-12-10 21:38:16 +0900

    修正しました。

--- numpy.py
+++ numpy.py
@@ -1,87 +1,44 @@
-#問１
+#問１ ndarrayの生成
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 np.random.seed(0)
 
-x = np.linspace(0,5,100)
+x = np.linspace(0,5,100) #観測値 x: 初項0，末項5，項数100の等差数列
 
-noise = np.random.normal(0, 1, 100)
+noise = np.random.normal(0, 1, 100) #ノイズ noise: 標準正規分布に従う100個の乱数
 
 def y(x):
     """真の関数"""
-    return 3 - 5 * x + x ** 2
+    return 3 - 5 * x + x ** 2  #真の関数 y(x) = 3 - 5x + x^2の定義
 
 
 
+#問2 ndarrayの操作
 
-#問2
-t = y(x) + noise
+t = y(x) + noise #観測値xに対応する目標値t
 
-plt.scatter(x,t)
-
-
+plt.scatter(x,t) #matplotlib で可視化
 
 
-#問3
-x = np.array([1,2,3])
+
+#問3 ndarrayの結合
+
 X = (np.array([
-    [np.where(x > 0, 1, 1)],
-    [x],
-    [x ** 2]])).T
+    [np.where(x > 0, 1, 1)], #x0: 長さが x と同じで要素が全て1のndarray
+    [x],                     #x1: 観測値 x の各要素を1乗したndarray
+    [x ** 2]])).T            #x2: 観測値 x の各要素を2乗したndarray
 X
 
 
 
+#問4  線形代数
 
-#問4
-x = np.linspace(0,5,100)
-noise = np.random.normal(0, 1, 100)
+w0, w1, w2 = (np.linalg.det(np.dot(X.T, X))) * (X.T) * t #パラメータのベクトル 
+pred_y = w0 + w1 * x + w2 * x ** 2 #観測値 x, t をもとに真の関数 y を2次関数で回帰
 
-def y(x):
-    """真の関数"""
-    return 3 - 5 * x + x ** 2
-
-t = y(x) + noise
-
-X = (np.array([
-    np.ones_like(x),
-    x,
-    x ** 2])).T
-
-
-w0, w1, w2 = (np.linalg.det(np.dot(X.T, X))) * (X.T) * t
-pred_y = w0 + w1 * x + w2 * x ** 2
-
-plt.scatter(x, t)
-plt.plot(x, pred_y, linewidth=5, color="red", label="pred")
-plt.plot(x, y(x), linewidth=5, color="green", label="true")
-plt.legend()
-
-
-
-
-
-x = np.linspace(0,5,100)
-noise = np.random.normal(0, 1, 100)
-
-def y(x):
-    """真の関数"""
-    return 5 * np.sin(np.pi * x / 5)
-
-t = y(x) + noise
-
-X = (np.array([
-    np.ones_like(x),
-    x,
-    x ** 2])).T
-
-
-w0, w1, w2 = (np.linalg.det(np.dot(X.T, X))) * (X.T) * t
-pred_y = w0 + w1 * x + w2 * x ** 2
-
-
-plt.scatter(x, t)
+plt.scatter(x, t) #先ほどのグラフに真の関数(緑)と回帰曲線(赤)を重ねる
 plt.plot(x, pred_y, linewidth=5, color="red", label="pred")
 plt.plot(x, y(x), linewidth=5, color="green", label="true")
 plt.legend()
