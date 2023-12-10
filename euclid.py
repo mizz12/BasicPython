ab27353c7322192c813c9f6536ae8f07b66d6fcf
mizz12 <w.mizuno.23.10.1208@4nonome.com>
2023-12-10 16:25:26 +0900

    関数課題

--- euclid.py
+++ euclid.py
@@ -1,12 +1,12 @@
-
+#問3
 a = int(input("a の値を入力: "))
 b = int(input("b の値を入力: "))
 
 # TODO
 
-if(a > b):
-    b,a = a,b
 def euclid(a,b):
+     if(a > b):
+          b,a = a,b
      while b :
           c = b
           b = a % b
@@ -14,3 +14,27 @@ def euclid(a,b):
      return int(c)
     
 print(euclid(a,b))
+
+#問4
+
+def relatively_prime(a,b):
+     if euclid(a,b) == 1:
+          return bool(1)
+     else:
+          return bool(0)
+     
+print(relatively_prime(a,b))
+
+#エクストラ問題
+
+import random
+
+l = 0
+for i in range(100000):
+     a,b = random.sample(range(1, 10000), 2)
+     if relatively_prime(a,b) == True:
+          l += 1
+print(f"互いに素である確率は{l / 100000}です。")
+
+#答え合わせ用
+print(6 / (math.pi ** 2))
